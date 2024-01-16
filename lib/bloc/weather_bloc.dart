d16d58fef842d31d37f596988aa7158ec54ccacf
camcoder337 <camcoder337@gmail.com>
2024-01-16 18:24:25 +0100

    feat: bloc state management implementation

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -2,14 +2,32 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:weather/weather.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:equatable/equatable.dart';
 
 part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   WeatherBloc() : super(WeatherInitial()) {
-    on<WeatherEvent>((event, emit) {
-      // TODO: implement event handler
+    String? apiKey = dotenv.env['API_KEY'];
+    on<FetchWeater>((event, emit) async {
+      emit(WeatherLoading());
+      try{
+        WeatherFactory wf = WeatherFactory(apiKey!, language: Language.ENGLISH);
+
+        // Position position = await Geolocator.getCurrentPosition();
+
+        Weather weather = await wf.currentWeatherByLocation(
+            event.position.latitude,
+            event.position.longitude);
+        print(weather);
+        emit(WeatherSuccess(weather));
+      }catch(e){
+        emit(WeatherFailure());
+      }
     });
   }
 }
--- lib/bloc/weather_event.dart
+++ lib/bloc/weather_event.dart
@@ -1,4 +1,17 @@
 part of 'weather_bloc.dart';
 
-@immutable
-abstract class WeatherEvent {}
+sealed class WeatherEvent extends Equatable{
+  const WeatherEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FetchWeater extends WeatherEvent{
+  final Position position;
+
+  const FetchWeater(this.position);
+
+  @override
+  List<Object> get props => [position];
+}
