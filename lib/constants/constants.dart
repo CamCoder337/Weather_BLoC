17817cd6fdcf65852691c9f97201c795b8693cb9
camcoder337 <camcoder337@gmail.com>
2024-01-16 13:08:49 +0100

    feat: Simple constants Access

---
+++ lib/constants/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+//App Colors
+Color purple = const Color(0XFF673AB7);
+Color orange = const Color (0XFFFFAB40);
+class Constants {
+  Constants._();
+
+  //Colors
+  static const Color purple = Color(0XFF673AB7);
+  static const Color orange = Color(0XFFFFAB40);
+  static const Color white = Colors.white;
+
+  //Padding
+  static const EdgeInsetsGeometry bodyPadding = EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20);
+
+}
