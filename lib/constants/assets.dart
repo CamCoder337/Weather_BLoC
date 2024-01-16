986c9d61d28c0ed5a2717402be92fe736581be22
camcoder337 <camcoder337@gmail.com>
2024-01-16 13:08:27 +0100

    feat: Simple Assets Access

---
+++ lib/constants/assets.dart
@@ -0,0 +1,19 @@
+class Assets {
+  Assets._();
+  static const String folder = 'assets/';
+
+  static const defaultImage = '${folder}1.png';
+  static const thunderStormImage = '${folder}1.png';
+  static const smallRainyImage = '${folder}2.png';
+  static const heavyRainyImage = '${folder}3.png';
+  static const cloudySnowImage = '${folder}4.png';
+  static const cloudyMistImage = '${folder}5.png';
+  static const sunshineImage = '${folder}6.png';
+  static const cloudySunshineImage = '${folder}7.png';
+  static const cloudyImage = '${folder}8.png';
+  static const cloudyWindImage = '${folder}9.png';
+  static const dawnImage = '${folder}11.png';
+  static const moonImage = '${folder}12.png';
+  static const maxTempIamge = '${folder}13.png';
+  static const minTempIamge = '${folder}14.png';
+}
